51b5cf1b0a0d465165988ba6279a0efe19d20bd6
Mohil <30442978+lbcmk@users.noreply.github.com>
2023-01-19 16:00:22 +0000

    Refactored Code

--- test2.py
+++ test2.py
@@ -3,17 +3,21 @@ from time import sleep, time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO_PINS = [17, 27, 22, 10]
-GPIO_DP = [25, 12, 16, 19]
-GPIO_LED = [8, 20, 6, 5, 9, 7, 13, 11]
+GPIO_BUTTON = [17, 27, 22, 10]
+# [D4, D3, D2, D1]
+GPIO_DISPLAY = [25, 12, 16, 19]
+# [A, B, C, D, E, F, G, DP]
+GPIO_SEGMENT = [8, 20, 6, 5, 9, 7, 13, 11]
 
-for i in GPIO_PINS:
+ENABLE_SCROLL = TRUE
+
+for i in GPIO_BUTTON:
     GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     
-for i in GPIO_DP:
+for i in GPIO_DISPLAY:
     GPIO.setup(i, GPIO.OUT)
     
-for i in GPIO_LED:
+for i in GPIO_SEGMENT:
     GPIO.setup(i, GPIO.OUT)
 
 songs = [0, 1, 2, 3, 4]
@@ -27,18 +31,18 @@ e = {
 }
 
 def writeLetter(digit, letter, scroll=False):
-    for i in GPIO_DP:
+    for i in GPIO_DISPLAY:
         GPIO.output(i, False)
         
-    GPIO.output(GPIO_DP[digit], True)
+    GPIO.output(GPIO_DISPLAY[digit], True)
 
     for i in range(len(e[letter])):
-        GPIO.output(GPIO_LED[i], e[letter][i])
+        GPIO.output(GPIO_SEGMENT[i], e[letter][i])
 
-    if(digit == 1 and scroll == False):
-        GPIO.output(GPIO_LED[7], False)
-    if(digit == 0 and scroll == True):
-        GPIO.output(GPIO_LED[7], False)
+    if(digit == 1 and scroll == False and ENABLE_SCROLL == True):
+        GPIO.output(GPIO_SEGMENT[7], False)
+    if(digit == 0 and scroll == True and ENABLE_SCROLL == True):
+        GPIO.output(GPIO_SEGMENT[7], False)
 
 
 timecodes = [0, 0, 0, 0, 0]
@@ -48,20 +52,20 @@ def Timer(id, cd=0.25):
         return True
     return False
 
-scr = True
+scr = False
 
 try:
     while True:    
-        if(GPIO.input(GPIO_PINS[0]) == False):
+        if(GPIO.input(GPIO_BUTTON[0]) == False):
             if(Timer(0)):
                 songs = [songs[1], songs[2], songs[3], songs[4], songs[0]]
-        if(GPIO.input(GPIO_PINS[1]) == False):
+        if(GPIO.input(GPIO_BUTTON[1]) == False):
             if(Timer(1)):
                 songs = [songs[4], songs[0], songs[1], songs[2], songs[3]]
-        if(GPIO.input(GPIO_PINS[2]) == False):
+        if(GPIO.input(GPIO_BUTTON[2]) == False):
             if(Timer(2)):
                 songs = [songs[1], songs[0], songs[2], songs[3], songs[4]]
-        if(GPIO.input(GPIO_PINS[3]) == False):
+        if(GPIO.input(GPIO_BUTTON[3]) == False and ENABLE_SCROLL == True):
             if(Timer(3)):
                 scr = not scr
                 # timecodes[4] = time()
@@ -72,7 +76,7 @@ try:
             writeLetter(i, songs[i+int(scr)], scr)
             sleep(0.001)   
 except:
-    for i in GPIO_DP:
+    for i in GPIO_DISPLAY:
         GPIO.output(i, False)
     GPIO.cleanup()
     print("Program Broke")
